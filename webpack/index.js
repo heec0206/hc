8babff36ac86a4b5fbcb0bfc3f968f0e94d0f417
hc <hc@naver.com>
2020-04-20 12:39:30 +0900

    commit

---
+++ webpack/index.js
@@ -0,0 +1,3 @@
+import hello_word from "./source/hello.js";
+import world_word from "./source/world.js";
+document.querySelector("#root").innerHTML = hellow_word + ' ' + world_word;
---
+++ webpack/world.js
@@ -0,0 +1,2 @@
+import hello_word from "./source/hello.js";
+import world_word from "./source/world.js";
