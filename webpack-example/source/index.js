5d0313f5a61cee23cf9c57480266731809034d74
hcchoi <hcchoi@wbsoft.kr>
2020-09-02 17:27:28 +0900

    sdfgsdfgsdfgsdfggdfsdfggdfxczvcvxvxcz

--- webpack-example/source/index.js
+++ webpack-example/source/index.js
@@ -1,3 +1,6 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
+import css form './style.css';
 document.querySelector("#root").innerHTML = hello_word + ' ' + world_word;
+
+console.log('css',css);
--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -1,10 +1,18 @@
 
 const path = require('path');
 module.exports = {
-  mode : "production",
+  mode : "development",
   entry : "./source/index.js",
   output : {
     path : path.resolve(__dirname, "publish"),
     filename : 'index_bundle.js'
+  },
+  module : {
+    rules : [
+      test : /\.css$/,
+      use : [
+        'css-loader'
+      ]
+    ]
   }
 }
