88ccddf308d1cf4ad8b253cefa77bbcd23c2af21
hcchoi <hcchoi@wbsoft.kr>
2020-09-02 17:34:46 +0900

    cfvbsdfsdfsdfg

--- webpack-example/source/index.js
+++ webpack-example/source/index.js
@@ -1,6 +1,6 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
-import css form './style.css';
+import css form './wbCommon.css';
 document.querySelector("#root").innerHTML = hello_word + ' ' + world_word;
 
 console.log('css',css);
