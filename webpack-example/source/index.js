f4b1aab0fc091d3d7f73d939179331e52b934721
hcchoi <hcchoi@wbsoft.kr>
2020-09-03 17:48:21 +0900

    asdzxcvx

--- webpack-example/source/index.js
+++ webpack-example/source/index.js
@@ -1,5 +1,7 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
+import _ from "loadsh";
 import css from './wbCommon.css';
-document.querySelector("#root").innerHTML = hello_word + ' ' + world_word + ' @';
+document.querySelector("#root").innerHTML = _.join( [hellow_word, world_word], '' );
+
 console.log('css',css);
