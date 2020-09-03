9bb9133ba0b5f6defdd20fafafb22fb2bcd43f52
hcchoi <hcchoi@wbsoft.kr>
2020-09-03 10:16:41 +0900

    node_moduels 현행화 및 webpack 모듈화

--- webpack-example/source/index.js
+++ webpack-example/source/index.js
@@ -2,4 +2,4 @@ import hello_word from "./hello.js";
 import world_word from "./world.js";
 //import css form './wbCommon.css';
 document.querySelector("#root").innerHTML = hello_word + ' ' + world_word;
-//console.log('css',css);
+console.log('ggg');
