90a6b2fe3008a29bccb13a7da2d6402191deddcb
hcchoi <hcchoi@wbsoft.kr>
2020-09-03 17:27:23 +0900

    asdasd

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
   //mode : "production",
@@ -6,7 +7,7 @@ module.exports = {
     index : "./source/index.js",
     about : "./source/about.js",
   },
-  
+
   output : {
     path: path.resolve(__dirname, 'publish'),
     filename : "[name]_bundle.js"
@@ -23,5 +24,19 @@ module.exports = {
       },
     ],
   },
+
+  plugins : [
+    new HtmlWebpackPlugin({
+      template : "./source/index.html",
+      filename : "./index.html",
+      chunks : ['index'],
+    }),
+
+    new HtmlWebpackPlugin({
+      template : "./source/about.html",
+      filename : "./about.html",
+      chunks : ['about'],
+    }),
+  ]
 
 };
